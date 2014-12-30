f8e7516b3d63b4d34391220b8a2ac5266eaa8133
Chris Loew <cloew123@gmail.com>
2014-12-30 11:19:59 -0700

    Added core Egg loading functionality

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='leggs',
+      version='0.1',
+      description='Python Language Eggs',
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      #url='http://www.python.org/sigs/distutils-sig/',
+      packages=['leggs'],
+     )
