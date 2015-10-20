ea3ad11be4e7648c4b135bc9137cb55ccf932895
Chris Loew <cloew123@gmail.com>
2015-10-20 10:25:22 -0600

    Updated the Egg Loader to use the latest KaoFactory

--- leggs/egg_loader.py
+++ leggs/egg_loader.py
@@ -1,27 +1,23 @@
 from .symbol import Symbol
 from .word import Word
 
-from kao_factory.data_source_factory import DataSourceFactory
-from kao_factory.factory import Factory
-from kao_factory.Parameter.complex_parameter import ComplexParameter
-from kao_factory.Parameter.primitive_parameter import PrimitiveParameter
+from kao_factory import DataBoundFactory, Factory, FieldArg, StaticArg
 from kao_factory.Source.json_source import JsonSource
 
-word_parameters = [PrimitiveParameter("concept"),
-                   PrimitiveParameter("text")]
+form_parameters = [FieldArg("concept"),
+                   FieldArg("text")]
                    
-SymbolFactory = Factory(Symbol, word_parameters)
-WordFactory = Factory(Word, word_parameters)
+SymbolFactory = Factory(Symbol, *form_parameters)
+WordFactory = Factory(Word, *form_parameters)
 
 class Egg:
-    def __init__(self, language, symbols, words):
+    def __init__(self, language, symbols=[], words=[]):
         self.language = language
         self.symbols = symbols
         self.words = words
         
-parameters = [PrimitiveParameter("language"), ComplexParameter("symbols", SymbolFactory.loadAll, optional=True, default=[]),
-                                              ComplexParameter("words", WordFactory.loadAll, optional=True, default=[])]
+EggFactory = Factory(Egg, FieldArg("language"), symbols=SymbolFactory.LoadAllArg("symbols"), words=WordFactory.LoadAllArg("words"))
 
 def LoadEggs(eggFilename, encoding="utf8"):
     """ Loads the eggs from the file and returns them """
-    return DataSourceFactory(Egg, parameters, JsonSource(eggFilename, encoding=encoding)).loadAll()
+    return DataBoundFactory(EggFactory, JsonSource(eggFilename, encoding=encoding)).loadAll()
